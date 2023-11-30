e408a097866ee9760ac45fe63dd3a98621fed4e2
shibo1128 <s.wang20010618@gmail.com>
2023-11-29 22:40:55 -0500

    update mecanum code 30th Nov

--- partial_code/car_movement_mecanum/mecanum_controller.cpp
+++ partial_code/car_movement_mecanum/mecanum_controller.cpp
@@ -20,14 +20,14 @@
 MecanumController::MecanumController(int FLPIN1, int FLPIN2, int FRPIN1, int FRPIN2,
                                      int BLPIN1, int BLPIN2, int BRPIN1, int BRPIN2)
 {
-    FL_PIN1=FLPIN1;
-    FR_PIN1=FRPIN1;
-    BL_PIN1=BLPIN1;
-    BR_PIN1=BRPIN1;
-    FL_PIN2=FLPIN2;
-    FR_PIN2=FRPIN2;
-    BL_PIN2=BLPIN2;
-    BR_PIN2=BRPIN2;
+    FL_DIR_PIN=FLPIN1;
+    FR_DIR_PIN=FRPIN1;
+    BL_DIR_PIN=BLPIN1;
+    BR_DIR_PIN=BRPIN1;
+    FL_SPEED_PIN=FLPIN2;
+    FR_SPEED_PIN=FRPIN2;
+    BL_SPEED_PIN=BLPIN2;
+    BR_SPEED_PIN=BRPIN2;
 
     //Initialize PIN mode
     pinMode(FL_PIN1,OUTPUT);
--- partial_code/car_movement_mecanum/mecanum_controller.h
+++ partial_code/car_movement_mecanum/mecanum_controller.h
@@ -20,14 +20,15 @@
 class MecanumController
 {
     private:
-        int FL_PIN1;
-        int FR_PIN1;
-        int BL_PIN1;
-        int BR_PIN1;
-        int FL_PIN2;
-        int FR_PIN2;
-        int BL_PIN2;
-        int BR_PIN2;
+    // Direction pin will be digital output, which with a NOT gate to control 2 directions ports on L298N.
+        int FL_DIR_PIN;
+        int FR_DIR_PIN;
+        int BL_DIR_PIN;
+        int BR_DIR_PIN;
+        int FL_SPEED_PIN;
+        int FR_SPEED_PIN;
+        int BL_SPEED_PIN;
+        int BR_SPEED_PIN;
 
     public:
         //Initial the PINs
