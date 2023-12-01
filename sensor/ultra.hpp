8ffc564d5e4238f6b7c5da962a5a17cee6a98201
Songhao Huang <96879529+KANZEZ@users.noreply.github.com>
2023-12-01 14:42:20 -0500

    Update ultra.hpp

--- sensor/ultra.hpp
+++ sensor/ultra.hpp
@@ -1,6 +1,7 @@
 #ifndef __ULTRA__
 #define __ULTRA__
 
+#include "Arduino.h"
 #define TRIG_PIN_1 4
 #define ECHO_PIN_1 5
 #define TRIG_PIN_2 4
@@ -11,8 +12,14 @@ volatile float dist_1 = 0;
 volatile float dist_2 = 0;
 
 
-void Ultra_init(){
-    
+unsigned int UltrasonicSensor::getDistance() {
+  digitalWrite(TRIG_PIN_1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_1, LOW);
+
+  unsigned long duration = pulseIn(ECHO_PIN_1, HIGH);
+
+ dist_1 = duration * 0.034 / 2;
 }
 
 
