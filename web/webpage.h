0b12d8edaae2c3b2b207ea2912aa7b7efad08ad7
shibo1128 <s.wang20010618@gmail.com>
2023-12-05 12:22:48 -0500

    add webpage code using html lin

---
+++ web/webpage.h
@@ -0,0 +1,9 @@
+const char body[] PROGMEM = R"===(
+ <!DOCTYPE html>  
+ <html><body>        
+ <h1>
+ <a href="/H">Turn ON</a> LED.<br>
+ <a href="/L">Turn OFF</a> LED.<br>
+ </h1>
+ </body></html>  
+)===";
